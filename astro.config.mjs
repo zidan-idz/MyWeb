859248085517830afa43ddc9cf3ced6d6d866afc
Zidan IDz <virusbootloop@gmail.com>
2026-01-05 14:46:00 +0800

    Initial Commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,28 @@
 // @ts-check
+// import { defineConfig } from 'astro/config';
+// import node from '@astrojs/node';
+// import tailwindcss from '@tailwindcss/vite';
+
+// // Dokumentasi Konfigurasi: https://astro.build/config
+// export default defineConfig({
+//   output: 'server',
+//   adapter: node({
+//     mode: 'standalone',
+//   }),
+//   vite: {
+//     plugins: [tailwindcss()]
+//   }
+// });
+
+// @ts-check
 import { defineConfig } from 'astro/config';
-import node from '@astrojs/node';
+import vercel from '@astrojs/vercel/serverless'; // Import adapter vercel
 import tailwindcss from '@tailwindcss/vite';
 
 // Dokumentasi Konfigurasi: https://astro.build/config
 export default defineConfig({
   output: 'server',
-  adapter: node({
-    mode: 'standalone',
-  }),
+  adapter: vercel(), // Gunakan adapter vercel
   vite: {
     plugins: [tailwindcss()]
   }
